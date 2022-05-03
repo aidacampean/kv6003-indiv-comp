335e8f4ce654c1475299dc7f0de0d52b6c026fa0
Aida Campean <aida@LAPTOP-E1C0BVC2.localdomain>
2022-05-03 22:00:38 +0100

    Add invite people back for collaborators

--- resources/views/planner/itinerary.blade.php
+++ resources/views/planner/itinerary.blade.php
@@ -5,9 +5,8 @@
         <h5>Tasks: <span class="badge badge-primary">{{ $data['tasks'][0]['task1'] }}</span> <span class="badge badge-info">{{ $data['tasks'][0]['task2'] }}</span></h5>
     @endif
 
-    @if ($data['user_id'] == Auth::user()->id)
-        <a class="align btn green text-white" href="{{ route('invite', ['id' => $data['id']]) }}">+ INVITE PEOPLE</a>
-    @endif
+      <a class="align btn green text-white" href="{{ route('invite', ['id' => $data['id']]) }}">+ INVITE PEOPLE</a>
+
     <create-itinerary :trip='@json($data)' />
     <p>
 </p>
